8c93a4f8a02630be290d8f63170363cb2f549e1c
Naum Krivoruk <naum.krivoruk@gmail.com>
2013-12-18 11:51:09 +0200

    myCtrl added

---
+++ app/scripts/controllers/my.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('tkApp')
+  .controller('MyctrlCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
